0ffe3891c06204a0f323cc5fb07941e2c2de4d17
Ильфат Сафиуллин <ilfat03031997@gmail.com>
2026-08-13 22:10:23 +0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -18,35 +18,32 @@ if (!process.env.CI) {
  */
 export default defineConfig({
   testDir: './tests',
-  timeout: 60000, // 60 секунд на каждый тест
   /* Run tests in files in parallel */
   fullyParallel: false,
-  workers: 1,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  //workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['line'], ['allure-playwright']],
+  reporter: [['html',], ['line',], ['allure-playwright',],],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BACK_URL
-  
+    baseURL: process.env.BACK_URL,
 
-      /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    screenshot: 'only-on-failure',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'] }
     },
+  
 /*
     {
       name: 'firefox',
