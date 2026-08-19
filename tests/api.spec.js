acaa43e9681226333c9dc685d0eda7e66962b55f
unknown <ilfat03031997@gmail.com>
2026-08-19 23:59:05 +0300

    исправления ошибок после ревью

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -10,7 +10,7 @@ test.beforeAll('01 - создание новой сессии участника
 
     const Accept = 'application/json';
 
-    headers = await api.challenger.post(Accept);
+    const headers = await api.challenger.post(Accept);
 
     token = headers['x-challenger'];
 
@@ -20,7 +20,7 @@ test.beforeAll('01 - создание новой сессии участника
 test('02 - получение списка challenges', { tag: '@get' }, async ({ api }) => {
 
     const Accept = 'application/json';
-    ({ body, headers, statuscode } = await api.challenges.get(token, Accept));
+    const { body, headers, statuscode } = await api.challenges.get(token, Accept);
 
     expect(body.challenges.length).toBeLessThanOrEqual(100);
     expect(headers['x-challenger']).toEqual(token);
@@ -32,9 +32,10 @@ test('12 - POST-запрос для создания задачи, но пров
 
     let todo = new ToDoBuilder().withTitle().withDoneStatus().withDescription({ length: 201 }).build();
     const Accept = 'application/json';
-    ({ body, headers, statuscode } = await api.todos.post(token, todo, Accept));
+    const { body, headers, statuscode } = await api.todos.post(token, todo, Accept);
 
     expect(statuscode).toEqual(422);
+    expect(headers['x-challenger']).toEqual(token);
     expect(body.errorMessages).toContain("Failed Validation: Maximum allowable length exceeded for description - maximum allowed is 200");
 });
 
@@ -44,17 +45,18 @@ test('13 - POST-запрос для создания задачи с указа�
 
     let todo = new ToDoBuilder().withTitle({ length: 50 }).withDoneStatus().withDescription({ length: 200 }).build();
     const Accept = 'application/json';
-    ({ body, headers, statuscode } = await api.todos.post(token, todo, Accept));
+    const { body, headers, statuscode } = await api.todos.post(token, todo, Accept);
 
     expect(statuscode).toEqual(201);
     expect(body.title).toEqual(todo.title);
+    expect(headers['x-challenger']).toEqual(token);
     expect(body.description).toEqual(todo.description);
 });
 
 test('04 - получение ошибки 404 списка заданий', { tag: '@get' }, async ({ api }) => {
 
     const Accept = '*/*';
-    ({ statuscode, headers } = await api.todo.get(token,Accept));
+    const { statuscode, headers } = await api.todo.get(token,Accept);
 
     expect(statuscode).toEqual(404);
     expect(headers['x-challenger']).toEqual(token);
@@ -65,7 +67,7 @@ test('06 - получение списка конкретного задания
     const id = 10000;
     const Accept = 'application/json';
 
-    ({ headers, statuscode } = await api.todos.getByid(token, id, Accept));
+    const { headers, statuscode } = await api.todos.getByid(token, id, Accept);
 
     expect(statuscode).toEqual(404);
     expect(headers['x-challenger']).toEqual(token);
